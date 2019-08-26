788ab0d81a2960e7fe752f3ae94885767018b84d
Kai-Tam <kai.k_632@icloud.com>
2019-08-26 20:32:46 +0900

    Initial Commit

---
+++ HelloWorldd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldd
+//
+//  Created by 小泉　開 on 2019/08/26.
+//  Copyright © 2019 jp.techachademy.KaiTam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
